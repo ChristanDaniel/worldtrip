d4a3f135d4e2659ce79035b7fdaea69a008b7688
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-27 18:04:23 -0300

    componente caracteristica criado

---
+++ src/components/Caracteristicas/Caracteristica.tsx
@@ -0,0 +1,10 @@
+import { Flex, Image, Text, useBreakpointValue } from "@chakra-ui/react";
+
+
+export default function Caracteristica() {
+    return (
+        <Flex direction={["row","column"]} align="center" justify="center">
+
+        </Flex>
+    )
+}
