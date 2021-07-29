0c4243cb3b4871623f4570e45fda4bf7dad759e7
ChristianDaniel <christian_daniel04@hotmail.com>
2021-07-29 15:02:59 -0300

    add head no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,31 @@
 import { Flex } from '@chakra-ui/react'
 
-import { Header } from '../components/Header/index'
+import Head from "next/head";
+
+import Caracteristicas from "../components/Caracteristicas";
+import {Header} from "../components/Header";
+import Separador from "../components/Separador";
 import { Banner } from '../components/Banner/index'
 
 
 export default function Home() {
   return(
     <Flex direction="column">
+  
+      <Head>
+      <title>WorldTrip - Home</title>
+          <meta property="og:image" content="/ogimage.png" />
+          <meta property="og:image:secure_url" content="/ogimage.png" />
+          <meta name="twitter:image" content="/ogimage.png" />
+          <meta name="twitter:image:src" content="/ogimage.png" />
+          <meta property="og:title" content="WorldTrip" />
+          <meta name="twitter:title" content="WorldTrip" />
+    </Head>
+
       <Header />
       <Banner />
+      <Caracteristicas />
+      <Separador />
     </Flex>
 
   )
