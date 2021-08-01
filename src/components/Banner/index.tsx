0da5b1fb5dc90f0a2f248c8ac3d6cb41dc77648f
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-01 13:52:00 -0300

    alteração no component banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,14 +1,25 @@
-import { Flex, Image } from '@chakra-ui/react'
-
+import { Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 export function Banner() {
     return(
         <Flex
-         w="100%"
-         h="335px"
-         bgImage="url('/banner.jpg')"
+        w="100%"
+        h={["163px", "250px", "250px", "335px"]}
+        bgImage="url('/banner.jpg')"
+        bgPosition={["100% 20%","100% 20%","100% 30%"]}
+        bgRepeat="no-repeat"
+        bgSize="cover"
         >
-
+         <Flex justify={['center', 'space-between']} align="center" w="100%" mx="auto" px={["4","10","15","20","36"]}>
+         <div>
+            <Heading color="gray.100" fontWeight="500" fontSize={["xl", "2xl", "2xl", "2xl", "4xl"]}>
+                5 continentes,<br />infinitas possibilidades.
+            </Heading>
+            <Text color="gray.300" mt="5" fontSize={["0.8rem","xl"]} maxW={["100%", "100%", "100%", "550px"]}>
+             Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </div>
+         </Flex>
         </Flex>
     )
 }
