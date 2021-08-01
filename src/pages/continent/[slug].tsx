1ff9778728c794e35145d49e74d213ead3f50050
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-01 14:02:30 -0300

    alteração noa page Continent

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -3,6 +3,11 @@ import Loading from "../../components/Loading";
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 
+import Cities from "../../components/Cities";
+import Content from "../../components/Content";
+import ContinentBanner from "../../components/ContinentBanner";
+import { Header } from "../../components/Header";
+
 export interface ContinentProps {
     continent: {
       slug: string;
@@ -41,6 +46,13 @@ export default function Continent({continent}: ContinentProps) {
                 <meta property="og:image" content={continent.banner_image} />
                 <meta property="og:image:secure_url" content={continent.banner_image} />
             </Head>
+            <Header />
+            <ContinentBanner continent={continent} />
+
+            <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
+                <Content continent={continent} />
+                <Cities continent={continent} />
+            </Flex>
         </Flex>
     )
 }
