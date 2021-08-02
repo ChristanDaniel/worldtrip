2c5d84062c774ffc6feb5c9c60459617e52d8ec3
ChristianDaniel <christian_daniel04@hotmail.com>
2021-08-02 15:20:38 -0300

    add primic na page slug

--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -1,7 +1,11 @@
 import { Flex } from "@chakra-ui/react";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Loading from "../../components/Loading";
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
+
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from '@prismicio/client';
 
 import Cities from "../../components/Cities";
 import Content from "../../components/Content";
@@ -28,31 +32,43 @@ export interface ContinentProps {
 }
 
 export default function Continent({continent}: ContinentProps) {
-    const router = useRouter();
-    if (router.isFallback) {
-      return <Loading />
-    }
-    return (
-        <Flex direction="column">
-            <Head>
-                <title>WorldTrip - {continent.title}</title>
+  const router = useRouter();
+  if (router.isFallback) {
+    return <Loading />
+  }
+  return (
+      <Flex direction="column">
+          <Head>
+              <title>WorldTrip - {continent.title}</title>
 
-                <meta property="og:title" content={`WorldTrip ${continent.title}`} />
-                <meta property="og:description" content={continent.description} />
-                <meta name="twitter:title" content={`WorldTrip ${continent.title}`} />
+              <meta property="og:title" content={`WorldTrip ${continent.title}`} />
+              <meta property="og:description" content={continent.description} />
+              <meta name="twitter:title" content={`WorldTrip ${continent.title}`} />
 
-                <meta name="twitter:image" content={continent.banner_image} />
-                <meta name="twitter:image:src" content={continent.banner_image} />
-                <meta property="og:image" content={continent.banner_image} />
-                <meta property="og:image:secure_url" content={continent.banner_image} />
-            </Head>
-            <Header />
-            <ContinentBanner continent={continent} />
+              <meta name="twitter:image" content={continent.banner_image} />
+              <meta name="twitter:image:src" content={continent.banner_image} />
+              <meta property="og:image" content={continent.banner_image} />
+              <meta property="og:image:secure_url" content={continent.banner_image} />
+          </Head>
+          <Header />
+          <ContinentBanner continent={continent} />
 
-            <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
-                <Content continent={continent} />
-                <Cities continent={continent} />
-            </Flex>
-        </Flex>
-    )
+          <Flex direction="column" maxW="1160px" mx="auto" mb="10" px="1rem">
+              <Content continent={continent} />
+              <Cities continent={continent} />
+          </Flex>
+      </Flex>
+  )
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const continents = await prismic.query([
+    Prismic.Predicates.at('document.type', 'continent'),
+  ]);
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
